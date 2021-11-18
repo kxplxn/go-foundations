e51a5b5abad911899defa61418ebde4b3a0f151c
Alican Demirtas <alcadeta@icloud.com>
2021-11-18 18:02:36 +0000

    3.2 Control/If

--- 03-control.go
+++ 03-control.go
@@ -5,24 +5,39 @@ import "fmt"
 func control() {
 	fmt.Println("\n\n_CONTROL_")
 
-	forLong()
-	forShort()
+	forLoop()
+	ifStatement()
 }
 
-func forLong() {
-	fmt.Println("\nFor (Long):")
+func forLoop() {
+	fmt.Println("\nFor Loop:")
 
+	fmt.Print("Long: ")
 	i := 1
 	for i <= 10 {
 		fmt.Printf("%v ", i)
 		i++
 	}
 	fmt.Println()
-}
 
-func forShort() {
-	fmt.Println("\nFor (Short):")
+	fmt.Print("Short: ")
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("%v ", i)
 	}
+	fmt.Println()
+}
+
+func ifStatement() {
+	fmt.Println("\nIf:")
+
+	runIf := func(number int) {
+		if number%2 == 0 {
+			fmt.Printf("%d is EVEN.\n", number)
+		} else {
+			fmt.Printf("%d is ODD.\n", number)
+		}
+	}
+
+	runIf(4)
+	runIf(5)
 }
