ba35e1504358bfd5628ff894846b9b852515e009
Alican Demirtas <alcadeta@icloud.com>
2021-11-18 15:24:35 +0000

    2.1 Variables/Basics

--- 01-types.go
+++ 01-types.go
@@ -2,23 +2,34 @@ package main
 
 import "fmt"
 
+func types() {
+	println("\n\n_TYPES_")
+
+	numbers()
+	strings()
+	booleans()
+}
+
 func numbers() {
 	fmt.Println("\nNumbers:")
+
 	fmt.Println("1 + 1 =", 1+1)
 	fmt.Println("1.0 + 1.0 =", 1.0+1.0)
 }
 
 func strings() {
 	fmt.Println("\nStrings:")
+
 	fmt.Println("len(\"Hello World\") =", len("Hello World"))
 	fmt.Println("\"HelloWorld\"[1] =", "HelloWorld"[1])
 }
 
 func booleans() {
 	fmt.Println("\nBooleans:")
-	fmt.Println("true && true =", true && true)
-	fmt.Println("true && false =", true && false)
-	fmt.Println("true || true =", true || true)
-	fmt.Println("true || false =", true || false)
-	fmt.Println("!true =", !true)
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
-func types() {
-	numbers()
-	strings()
-	booleans()
-}
-
 func main() {
 	types()
+	variables()
 }
