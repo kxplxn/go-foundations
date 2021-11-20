f2d8ba2e92294b4175f72fa7b7f0ad8cae3e91cf
Alican Demirtas <alicandev@icloud.com>
2021-11-20 00:17:41 +0000

    01030101 Functions

---
+++ functions.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func avg(x []float64) float64 {
+	total := 0.0
+	for _, val := range x {
+		total += val
+	}
+	return total / float64(len(x))
+}
+
+func functionsDemo() {
+	fmt.Println("Running `functionsDemo`...")
+	avgRes := avg([]float64{43.0, 34.0, 21.0, 52.0})
+	fmt.Printf("Result of `avg` is `%.2f`.\n", avgRes)
+}
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Learning GO at SkillSoft!")
+	functionsDemo()
 }
